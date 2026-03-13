c718233fa28de6f3e1f2268c02fc8a2ceab37a89
aiworkhackshop-design <ai.workhack.shop@gmail.com>
2026-03-13 20:36:53 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+import requests
+import random
+
+# 楽天商品取得
+def get_rakuten_items(app_id, keyword):
+    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+    params = {
+        "applicationId": app_id,
+        "keyword": keyword,
+        "hits": 5
+    }
+
+    res = requests.get(url, params=params)
+    data = res.json()
+
+    items = data["Items"]
+    item = random.choice(items)["Item"]
+
+    return {
+        "title": item["itemName"],
+        "url": item["itemUrl"],
+        "image": item["mediumImageUrls"][0]["imageUrl"]
+    }
+
+
+# 投稿文生成
+def generate_caption(item):
+    caption = f"""
+【おすすめ商品】
+
+{item['title']}
+
+気になる人はこちら👇
+{item['url']}
+
+#楽天 #おすすめ商品 #買ってよかった
+"""
+    return caption
+
+
+if __name__ == "__main__":
+    RAKUTEN_APP_ID = "YOUR_RAKUTEN_APP_ID"
+
+    item = get_rakuten_items(RAKUTEN_APP_ID, "便利グッズ")
+    caption = generate_caption(item)
+
+    print("タイトル:", item["title"])
+    print("URL:", item["url"])
+    print("投稿文:", caption)
