b5f89a80cf69eaab0960bb6150a728d7a9ff2f23
aiworkhackshop-design <ai.workhack.shop@gmail.com>
2026-03-14 12:50:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 ACCOUNT_ID = os.getenv("INSTAGRAM_BUSINESS_ACCOUNT_ID")
 ACCESS_TOKEN = os.getenv("FACEBOOK_PAGE_ACCESS_TOKEN")
 
+
 @app.route("/")
 def home():
-    return "Instagram自動投稿実行中"
+    return "Instagram Auto Post Running 🚀"
+
 
 @app.route("/post")
 def post():
@@ -17,13 +19,29 @@ def post():
     url = f"https://graph.facebook.com/v19.0/{ACCOUNT_ID}/media"
 
     data = {
-        "image_url": "https://picsum.photos/1080/1080",
-        "caption": "AIおすすめ商品ランキング",
+        "image_url": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
+        "caption": "AIおすすめ商品ランキング🔥",
         "access_token": ACCESS_TOKEN
     }
 
     r = requests.post(url, data=data)
-    return r.text
+    result = r.json()
+
+    if "id" not in result:
+        return result
+
+    creation_id = result["id"]
+
+    publish_url = f"https://graph.facebook.com/v19.0/{ACCOUNT_ID}/media_publish"
+
+    publish_data = {
+        "creation_id": creation_id,
+        "access_token": ACCESS_TOKEN
+    }
+
+    r2 = requests.post(publish_url, data=publish_data)
+
+    return r2.text
 
 
 if __name__ == "__main__":
