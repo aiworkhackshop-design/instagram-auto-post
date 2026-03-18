4698c54d75b59304021cd4af93013c5885b3d0bd
aiworkhackshop-design <ai.workhack.shop@gmail.com>
2026-03-18 12:57:20 +0900

    Update generate_video.js

--- generate_video.js
+++ generate_video.js
@@ -4,37 +4,46 @@ function generateVideo(product) {
   const safeTitle = product.title
     .replace(/'/g, "")
     .replace(/:/g, "")
-    .slice(0, 20);
+    .slice(0, 18);
 
   execSync(`
     ffmpeg -y -loop 1 -i ${imagePath} \
     -vf "
     scale=1080:1920,
-    zoompan=z='min(zoom+0.0015,1.4)':d=180,
 
-    drawbox=x=0:y=0:w=1080:h=500:color=black@0.7:t=fill,
-    drawbox=x=0:y=1500:w=1080:h=420:color=black@0.7:t=fill,
+    zoompan=z='min(zoom+0.002,1.3)':d=180,
+
+    drawbox=x=0:y=0:w=1080:h=350:color=black@0.6:t=fill,
 
     drawtext=fontfile=/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc:
     text='【保存しないと損】':
     fontcolor=yellow:
-    fontsize=90:
+    fontsize=85:
     x=(w-text_w)/2:
-    y=120,
+    y=80,
 
     drawtext=fontfile=/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc:
     text='${safeTitle}':
     fontcolor=white:
-    fontsize=65:
+    fontsize=60:
     x=(w-text_w)/2:
-    y=300,
+    y=200,
+
+    drawbox=x=0:y=1550:w=1080:h=300:color=black@0.6:t=fill,
 
     drawtext=fontfile=/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc:
-    text='👇プロフのリンクから見れる':
+    text='👇プロフのリンクから':
     fontcolor=white:
     fontsize=55:
     x=(w-text_w)/2:
-    y=1580,
+    y=1600,
+
+    drawtext=fontfile=/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc:
+    text='今すぐチェック':
+    fontcolor=yellow:
+    fontsize=70:
+    x=(w-text_w)/2:
+    y=1700,
 
     format=yuv420p
     " \
