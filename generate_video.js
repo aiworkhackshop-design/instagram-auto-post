438f6e0941ca83415e8085b2023ed820a37b3191
aiworkhackshop-design <ai.workhack.shop@gmail.com>
2026-03-18 12:53:42 +0900

    Update generate_video.js

--- generate_video.js
+++ generate_video.js
@@ -1,26 +1,43 @@
-import { execSync } from "child_process";
-import fs from "fs";
-import fetch from "node-fetch";
+function generateVideo(product) {
+  console.log("GENERATE VIDEO");
 
-const IMAGE_URL = "https://images.unsplash.com/photo-1542291026-7eec264c27ff";
-const OUTPUT = "output.mp4";
+  const safeTitle = product.title
+    .replace(/'/g, "")
+    .replace(/:/g, "")
+    .slice(0, 20);
 
-async function downloadImage() {
-  const res = await fetch(IMAGE_URL);
-  const buffer = await res.buffer();
-  fs.writeFileSync("image.jpg", buffer);
-}
-
-async function generateVideo() {
   execSync(`
-    ffmpeg -y -loop 1 -i image.jpg \
-    -vf "scale=1080:1920,zoompan=z='min(zoom+0.0015,1.2)':d=125" \
-    -t 5 -pix_fmt yuv420p ${OUTPUT}
+    ffmpeg -y -loop 1 -i ${imagePath} \
+    -vf "
+    scale=1080:1920,
+    zoompan=z='min(zoom+0.0015,1.4)':d=180,
+
+    drawbox=x=0:y=0:w=1080:h=500:color=black@0.7:t=fill,
+    drawbox=x=0:y=1500:w=1080:h=420:color=black@0.7:t=fill,
+
+    drawtext=fontfile=/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc:
+    text='【保存しないと損】':
+    fontcolor=yellow:
+    fontsize=90:
+    x=(w-text_w)/2:
+    y=120,
+
+    drawtext=fontfile=/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc:
+    text='${safeTitle}':
+    fontcolor=white:
+    fontsize=65:
+    x=(w-text_w)/2:
+    y=300,
+
+    drawtext=fontfile=/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc:
+    text='👇プロフのリンクから見れる':
+    fontcolor=white:
+    fontsize=55:
+    x=(w-text_w)/2:
+    y=1580,
+
+    format=yuv420p
+    " \
+    -t 6 -r 30 -c:v libx264 -pix_fmt yuv420p ${videoPath}
   `);
 }
-
-(async () => {
-  await downloadImage();
-  await generateVideo();
-  console.log("VIDEO CREATED");
-})();
